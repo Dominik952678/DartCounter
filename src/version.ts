54d966016214894fdc432f3cce13d1acc7c087d2
dominik <dominik.bejko@stud.h-da.de>
2026-08-28 12:55:01 +0200

    chore(release): bump version to v1.4.0

--- src/version.ts
+++ src/version.ts
@@ -1,5 +1,5 @@
 declare const __APP_VERSION__: string;
 declare const __BUILD_TIME__: string;
 
-export const APP_VERSION = typeof __APP_VERSION__ !== 'undefined' ? __APP_VERSION__ : 'v1.3.0';
+export const APP_VERSION = typeof __APP_VERSION__ !== 'undefined' ? __APP_VERSION__ : 'v1.4.0';
 export const BUILD_TIME = typeof __BUILD_TIME__ !== 'undefined' ? __BUILD_TIME__ : '23.08.2026 19:20';
